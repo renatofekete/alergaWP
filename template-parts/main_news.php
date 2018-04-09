65df73803a9b420e783e81eed4a8c4135f9d2d5e
renatofekete <renatofekete.91@gmail.com>
2018-04-09 21:37:04 +0200

    fresh

--- template-parts/main_news.php
+++ template-parts/main_news.php
@@ -1,8 +1,8 @@
 <div class="news">
-    <a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail('main-news')?>
-        <div class="news_text">
-        <h1><?php the_title(); ?></h1>
-        </div>
-    </a>
+	<a href="<?php the_permalink(); ?>">
+		<?php the_post_thumbnail('main-news')?>
+		<div class="news_text">
+		<h1><?php the_title(); ?></h1>
+		</div>
+	</a>
 </div>
