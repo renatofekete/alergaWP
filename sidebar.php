ef6620bd85d6d4c406066102076583151a8a2f72
renatofekete <renatofekete.91@gmail.com>
2018-04-19 11:34:01 +0200

    aside

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,32 @@
-<?php 
-  $cat = new WP_Query( array( 
-    'category__in'   => wp_get_post_categories( $post->ID ),
-    'posts_per_page' => 2,
-    'post__not_in'   => array( $post->ID )
+
+<div class="aside_news">
+  <div class="title section_title"><h3>Iz kategorije</h3></div>
+  <?php 
+  $cat = new WP_Query(array(
+    'category__in' => wp_get_post_categories($post->ID),
+    'posts_per_page' => 5,
+    'post__not_in' => array($post->ID)
   ));
-?>
-<?php if($cat->have_posts()) :
-  while ($cat->have_posts()) : $cat -> the_post(); 
-  the_title();
-endwhile;
-endif;
-wp_reset_query(); ?>
+  ?>
+  <?php if ($cat->have_posts()) :
+    while ($cat->have_posts()) : $cat->the_post();
+  get_template_part('template-parts/aside_news');
+  endwhile;
+  endif;
+  wp_reset_query(); ?>
+
+  <div class="title section_title"><h3>Najnovije</h3></div>
+  <?php 
+  $cat = new WP_Query(array(
+    'posts_per_page' => 5,
+    'post__not_in' => array($post->ID)
+  ));
+  ?>
+  <?php if ($cat->have_posts()) :
+    while ($cat->have_posts()) : $cat->the_post();
+  get_template_part('template-parts/aside_news');
+  endwhile;
+  endif;
+  wp_reset_query(); ?>
+
+</div><!-- aside_news -->
---
+++ template-parts/aside_news.php
@@ -0,0 +1,7 @@
+<div class="news">
+  <div class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+  <div class="meta_info">
+    <i class="far fa-clock"></i>
+    <p><?php echo get_the_date(); ?> | <?php echo get_the_time(); ?></p> 
+  </div>
+</div>
